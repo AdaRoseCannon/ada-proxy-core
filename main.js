5280c9f11d6cad48e82a6cdfbb7154ec93b0ec82
Ada Rose Edwards <ada@ada.is>
2014-07-13 20:57:06 -0700

    Updated proxy

--- main.js
+++ main.js
@@ -6,11 +6,13 @@ var http = require('http'),
 	forever = require('forever');
 	gith = require('gith');
 
-var port = options.port || 8080;
+var PORT = options.port || 8080;
 var p=process.argv.indexOf('-p');
 if(!!~p && process.argv[p+1]) {
-	port = process.argv[p+1];
+	PORT = process.argv[p+1];
 }
+
+var GITH_PORT = PORT + 1;
 
 var proxy = httpProxy.createProxyServer({});
 
@@ -28,23 +30,26 @@ var server = http.createServer(function(req, res) {
 
 	if (req.headers.host.match(/^githooks/i)) {
 		console.log('github webhook redirect');
+		proxy.web(req, res,{ target: "http://localhost:" + GITH_PORT });
 	}
 
 	for (var i=0;i<jobsLength;i++) {
 		var item = jobsArray[i];
 		if (req.headers.host.match(new RegExp(item.pattern))) {
-                        console.log('redirecting to:', item.target);
+			console.log('redirecting to:', item.target);
 			proxy.web(req, res,{ target: item.target });
 			break;
 		}
 	}
 });
 
-console.log("listening on port ", port);
-server.listen(port);
+console.log("listening on PORT ", PORT);
+server.listen(PORT);
 
-var gith = require( 'gith' ).create( port + 1 );
- 
+var gith = require( 'gith' ).create( GITH_PORT );
+
+console.log("Githooks listening on PORT ", GITH_PORT);
+
 gith().on( 'all', function( payload ) {
   console.log(payload);
 });
