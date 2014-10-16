6f79e3d22c31e0a3e1928117c6333eda4fca6445
Ada Rose <ada.edwards@ft.com>
2014-10-16 16:47:34 -0700

    Warning currently has poodle vuln

--- options.js
+++ options.js
@@ -6,18 +6,6 @@ module.exports = {
 	port: 8080,
 	https_port: 8443,
 	ssl_options: {
-		// This is the default secureProtocol used by Node.js, but it might be
-		// sane to specify this by default as it's required if you want to
-		// remove supported protocols from the list. This protocol supports:
-		//
-		// - SSLv2, SSLv3, TLSv1, TLSv1.1 and TLSv1.2
-		//
-		secureProtocol: 'SSLv23_method',
-
-		// Supply `SSL_OP_NO_SSLv3` constant as secureOption to disable SSLv3
-		// from the list of supported protocols that SSLv23_method supports.
-		//
-		secureOptions: constants.SSL_OP_NO_SSLv3,
 		key: fs.readFileSync("/home/ada/keys/ssl.key"),
 		cert: fs.readFileSync("/home/ada/keys/ssl.crt"),
 		ca: [
@@ -29,6 +17,3 @@ module.exports = {
 	repoURL: "https://github.com/AdaRoseEdwards/ada-proxy",
 	repoRef: "refs/heads/master"
 };
-
-
-module.exports.ssl_options.agent = new https.Agent(module.exports.ssl_options);
