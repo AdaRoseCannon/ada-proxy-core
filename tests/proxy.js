568df5cf3ae808b7f0e16cceb951e6d785076387
Ada Rose Edwards <ada@ada.is>
2014-12-26 10:28:59 -0500

    tweak tests

--- tests/proxy.js
+++ tests/proxy.js
@@ -1,8 +1,15 @@
 "use strict";
 
 var testReq = require('./setup');
+var express = require('express');
+var app = express();
 
+app.get('/proxy/', function(req, res){
+	res.setHeader("A-Proxied-Request", "Is Proxied");
+	res.end('hello world');
+});
 
+app.listen(8081);
 
 module.exports = new Promise(function (resolve) {
 	testReq
--- tests/setup.js
+++ tests/setup.js
@@ -3,15 +3,6 @@
 require('es6-promise').polyfill();
 
 var request = require('supertest');
-var express = require('express');
-var app = express();
-
-app.get('*', function(req, res){
-	res.setHeader("A-Proxied-Request", "Is Proxied");
-	res.end('hello world');
-});
-
-app.listen(8081);
 
 var options = {
 	port: 8080
